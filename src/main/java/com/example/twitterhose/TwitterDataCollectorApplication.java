65e309a1e0acd590f567b4bf3ecf2de897c7b3e7
madhusdhnn <madhusudhanan.p@gofrugal.com>
2019-11-16 19:21:38 +0530

    Setup Spring boot project with dependencies

---
+++ src/main/java/com/example/twitterhose/TwitterDataCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.twitterhose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterDataCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterDataCollectorApplication.class, args);
+	}
+
+}
