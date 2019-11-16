eadefa3575bcdbb3a922a634ade63f5b11cb2b18
madhusdhnn <madhusudhanan.p@gofrugal.com>
2019-11-16 23:53:26 +0530

    1. Refactor twitter api config file 2. Add kafka producer config

--- src/main/java/com/example/twitterhose/configurations/TwitterApiConfig.java
+++ src/main/java/com/example/twitterhose/configurations/TwitterApiConfig.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Component;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
-import javax.annotation.PostConstruct;
-
 @Component
 public class TwitterApiConfig {
 
@@ -14,22 +12,13 @@ public class TwitterApiConfig {
     private static final String ACCESS_TOKEN = "591795371-wrYKl7FqLaaZok8G132y0OFJRuwGvelz77leDdZ7";
     private static final String ACCESS_TOKEN_SECRET = "mZCwAJ9bylkvfV6nt1dPizUh3jhXnHmF4RKohcQfHSkP6";
 
-    private Configuration configuration;
-
-    @PostConstruct
-    public void setupTwitter4jConfiguration() {
+    public Configuration setupTwitter4jConfiguration() {
         final ConfigurationBuilder builder = new ConfigurationBuilder();
-        this.configuration = builder.setOAuthConsumerKey(CONSUMER_KEY)
+        return builder.setOAuthConsumerKey(CONSUMER_KEY)
                 .setOAuthConsumerSecret(CONSUMER_SECRET)
                 .setOAuthAccessToken(ACCESS_TOKEN)
                 .setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET)
                 .build();
-    }
-
-    public Configuration getTwitterConfiguration() {
-        if (this.configuration == null)
-            throw new RuntimeException("Twitter API key and token are not configured.");
-        return this.configuration;
     }
 
 }
